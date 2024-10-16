e3c88981e55fcd6431f45776b2bb70d912397cd9
Prince <bhakodiyaprince05@gmail.com>
2024-10-16 17:37:28 +0530

    url change

--- lib/core/Appconstant.dart
+++ lib/core/Appconstant.dart
@@ -1,10 +1,10 @@
 class AppConstants {
   AppConstants._();
   static const String serverUrl = "";
-  static const String socketURL = "http://3.6.160.162:2011/";
+  static const String socketURL = "http://13.126.106.41:2011/";
   // static const String socketURL = "https://sngmncg2-2011.inc1.devtunnels.ms";
-  static const String baseUrl = "http://3.6.160.162:2011/api";
+  static const String baseUrl = "http://13.126.106.41:2011/api";
   // static const String baseUrl = "https://sngmncg2-2011.inc1.devtunnels.ms/api";
-  static const String ONDCbaseUrl = "http://3.6.160.162:2011/api";
-  static const String ONDCUrl = "http://3.6.160.162:2011/";
+  static const String ONDCbaseUrl = "http://13.126.106.41:2011/api";
+  static const String ONDCUrl = "http://13.126.106.41:2011/";
 }
